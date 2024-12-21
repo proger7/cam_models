2ee312ec0d5b6f42e733b3bf27d362ef113c67b3
proger7 <quantumwise555@gmail.com>
2024-12-21 23:23:34 +0200

    task with tags widget with reloading page

--- functions.php
+++ functions.php
@@ -74,3 +74,94 @@ function custom_popup_scripts() {
     wp_enqueue_script('custom-popup-script', get_stylesheet_directory_uri() . '/js/popup.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'custom_popup_scripts');
+
+
+
+class Tags_Widget extends WP_Widget {
+    public function __construct() {
+        parent::__construct(
+            'tags_widget',
+            'Tags Widget',
+            array('description' => 'A widget to display a list of tags')
+        );
+
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_styles']);
+    }
+
+    public function enqueue_styles() {
+        if (is_active_widget(false, false, $this->id_base, true)) {
+            wp_enqueue_style('tags-widget-style', get_stylesheet_directory_uri() . '/css/tags-widget.css');
+        }
+    }
+
+    public function widget($args, $instance) {
+        $tags = [
+            "anal", "bdsm", "asian", "big ass", "blond", "african", "asmr"
+        ];
+
+        echo $args['before_widget'];
+        ?>
+        <div class="tags-sidebar">
+            <div class="tags-list" id="tagsList">
+                <?php foreach (array_slice($tags, 0, 5) as $tag): ?>
+                    <a class="tag" href="#" data-tag="<?php echo ltrim($tag, '#'); ?>"><?php echo $tag; ?></a>
+                <?php endforeach; ?>
+            </div>
+            <button class="more-btn" id="moreBtn">More...</button>
+        </div>
+
+        <script>
+            document.addEventListener("DOMContentLoaded", () => {
+                const tagsList = document.getElementById("tagsList");
+                const moreBtn = document.getElementById("moreBtn");
+                const tags = <?php echo json_encode($tags); ?>;
+                let visibleCount = 5;
+
+                const renderTags = () => {
+                    tagsList.innerHTML = "";
+                    tags.slice(0, visibleCount).forEach(tag => {
+                        const tagElement = document.createElement("a");
+                        tagElement.className = "tag";
+                        tagElement.href = "#";
+                        tagElement.textContent = tag;
+                        tagElement.dataset.tag = tag.replace("#", "");
+                        tagsList.appendChild(tagElement);
+                    });
+                    moreBtn.style.display = visibleCount >= tags.length ? "none" : "block";
+                };
+
+                moreBtn.addEventListener("click", () => {
+                    visibleCount += 5;
+                    renderTags();
+                });
+
+                tagsList.addEventListener("click", (event) => {
+                    if (event.target.classList.contains("tag")) {
+                        event.preventDefault();
+                        const selectedTag = event.target.dataset.tag;
+                        const url = new URL(window.location);
+                        url.searchParams.set("q", selectedTag);
+                        window.location = url;
+                    }
+                });
+
+                renderTags();
+            });
+        </script>
+        <?php
+        echo $args['after_widget'];
+    }
+
+    public function form($instance) {}
+
+    public function update($new_instance, $old_instance) {
+        return $new_instance;
+    }
+}
+
+
+
+function register_tags_widget() {
+    register_widget('Tags_Widget');
+}
+add_action('widgets_init', 'register_tags_widget');
--- page-cam-template.php
+++ page-cam-template.php
@@ -1,56 +1,79 @@
 <?php
-/**
+/*
  * Template Name: Cam Template
  */
-get_header();
 
-$tag = get_post_meta(get_the_ID(), 'cam_tag', true);
-?>
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
 
+$tag = isset($_GET['q']) ? sanitize_text_field($_GET['q']) : get_post_meta(get_the_ID(), 'cam_tag', true);
 
-<div class="camTemplateItem__wrapper" style="width:100%">
-    <div class="camTemplateItem__row" id="camContent"></div>
+get_header(); ?>
+
+<div <?php generate_do_attr( 'content' ); ?>>
+    <main <?php generate_do_attr( 'main' ); ?>>
+        <div class="camTemplateItem__wrapper" style="width:100%">
+            <div class="camTemplateItem__row" id="camContent"></div>
+        </div>
+
+        <script id="camContentTemplate" type="text/template">
+            <div class="camTemplateItem__item-col">
+                <a href="{{link}}" class="camTemplateItem">
+                    <span class="camTemplateItem__image">
+                        <img loading="lazy" src="{{image_url}}" alt="{{username}} webcam" class="camTemplateItem--image">
+                    </span>
+                    <span class="camTemplateItem__modelWrapper">
+                        <span class="camTemplateItem__modelInfoWrapper">
+                            <span class="camTemplateItem__modelInfo">
+                                <h2 class="camTemplateItem__modelName">{{username}}</h2>
+                            </span>
+                            <span class="camTemplateItem__modelEthnicity">
+                                <span class="camTemplateItem__name">{{age}}</span>
+                                <span class="camTemplateItem__value"> Year old </span>
+                                <span class="camTemplateItem__name">{{gender}}</span>
+                            </span>
+                        </span>
+                    </span>
+                </a>
+            </div>
+        </script>
+
+        <script>
+            var camApiConfig = {
+                'sortOrder': '',
+                'status': ['online'],
+                'q': '<?php echo esc_js($tag); ?>',
+                'gender': ['f', 'm'],
+                'tags': '',
+                'languages': '',
+                'ethnicity': '',
+                'hair': '',
+                'age': '',
+                'sites': [1],
+                'divTarget': 'camContent',
+                'divTemplate': 'camContentTemplate',
+                'limitPerPage': 10,
+                'allowPagination': true
+            };
+        </script>
+
+        <?php
+        do_action( 'generate_before_main_content' );
+
+        if ( generate_has_default_loop() ) {
+            while ( have_posts() ) :
+                the_post();
+                generate_do_template_part( 'page' );
+            endwhile;
+        }
+
+        do_action( 'generate_after_main_content' );
+        ?>
+    </main>
 </div>
 
-<script id="camContentTemplate" type="text/template">
-    <div class="camTemplateItem__item-col">
-        <a href="{{link}}" class="camTemplateItem">
-            <span class="camTemplateItem__image">
-                <img loading="lazy" src="{{image_url}}" alt="{{username}} webcam" class="camTemplateItem--image">
-            </span>
-            <span class="camTemplateItem__modelWrapper">
-                <span class="camTemplateItem__modelInfoWrapper">
-                    <span class="camTemplateItem__modelInfo">
-                        <h2 class="camTemplateItem__modelName">{{username}}</h2>
-                    </span>
-                    <span class="camTemplateItem__modelEthnicity">
-                        <span class="camTemplateItem__name">{{age}}</span>
-                        <span class="camTemplateItem__value"> Year old </span>
-                        <span class="camTemplateItem__name">{{gender}}</span>
-                    </span>
-                </span>
-            </span>
-        </a>
-    </div>
-</script>
-
-<script>
-    var camApiConfig = {
-        'sortOrder': '',
-        'status': ['online'],
-        'q': '<?php echo esc_js($tag); ?>',
-        'gender': ['f', 'm'],
-        'tags': '',
-        'languages': '',
-        'ethnicity': '',
-        'hair': '',
-        'age': '',
-        'sites': [1],
-        'divTarget': 'camContent',
-        'divTemplate': 'camContentTemplate',
-        'limitPerPage': 10,
-        'allowPagination': true
-    };
-</script>
-
-<?php get_footer(); ?>
+<?php
+do_action( 'generate_after_primary_content_area' );
+generate_construct_sidebars();
+get_footer();
