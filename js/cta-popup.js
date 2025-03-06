179f2bad712b1a3e89850098e758eb9f0d8eaa9c
proger7 <quantumwise555@gmail.com>
2025-03-06 13:07:46 +0200

    fix notification popup

--- js/cta-popup.js
+++ js/cta-popup.js
@@ -1,11 +1,23 @@
 jQuery(document).ready(function($) {
+    if (sessionStorage.getItem('ctaPopupShown')) {
+        return;
+    }
+    
     function showPopup() {
-        $('#cta-popup, #cta-popup-overlay').fadeIn();
+        $('#cta-popup').css({'display': 'block', 'opacity': '0'}).animate({opacity: 1}, 300);
+        sessionStorage.setItem('ctaPopupShown', 'true');
+        setTimeout(function() {
+            $('#cta-popup').animate({opacity: 0}, 300, function() {
+                $(this).css('display', 'none');
+            });
+        }, 10000);
     }
 
-    setTimeout(showPopup, 2000);
+    setTimeout(showPopup, 1500);
 
     $('.cta-popup-no, #cta-popup-overlay').on('click', function() {
-        $('#cta-popup, #cta-popup-overlay').fadeOut();
+        $('#cta-popup').animate({opacity: 0}, 300, function() {
+            $(this).css('display', 'none');
+        });
     });
 });
